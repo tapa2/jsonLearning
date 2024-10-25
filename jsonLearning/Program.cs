d2d37a932314ee9c77251cf350f2619bc17ba4c3
Lenovo <Lenovo@DESKTOP-2N1A3O0>
2024-10-25 19:22:55 +0300

    Add project files.

---
+++ jsonLearning/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+
+
+public class PublishingHouse
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; }
+}
+
+public class Book
+{
+
+    [JsonPropertyName("Title")]
+    public string Name { get; set; }
+    public PublishingHouse PublishingHouse { get; set; }
+}
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        string jsonContent = File.ReadAllText("books.json");
+        List<Book> books = JsonSerializer.Deserialize<List<Book>>(jsonContent);
+
+        Console.WriteLine("Books deserialized:");
+        foreach (var book in books)
+        {
+            Console.WriteLine($"Title: {book.Name}, Publishing House: {book.PublishingHouse.Name}");
+        }
+
+        var options = new JsonSerializerOptions
+        {
+            WriteIndented = true 
+        };
+
+        string serializedJson = JsonSerializer.Serialize(books, options);
+        File.WriteAllText("output.json", serializedJson);
+        Console.WriteLine("\nBooks serialized and saved to output.json");
+    }
+}
